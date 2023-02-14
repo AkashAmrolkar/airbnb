103bbd010f94b9abd0805a94b3288a5566dde651
akash <akashamrolkar10@gmail.com>
2023-02-14 23:06:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className='mt-4 border-gray-300'>Akash</h1>
+    </div>
+  )
+}
+
+export default App
